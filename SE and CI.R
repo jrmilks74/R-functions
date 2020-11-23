89963faedecc0bdbdf495352528652bd2851a0cb
Jim Milks <jrmilks@yahoo.com>
2020-11-23 10:39:14 -0500

    Update SE and CI.R

--- SE and CI.R
+++ SE and CI.R
@@ -1,21 +1,21 @@
 #Standard Error calculation for the specified vector
 se <- function(x){
   n <- length(x)
-  sd(x)/sqrt(n)
+  sd(x) / sqrt(n)
 }
 
 #Returns the 95% confidence intervals for the specified vector
 ci <- function(x, alpha){
         n <- length(x)
-        se <- sd(x)/sqrt(n)
-        c(mean(x)-qt(1-alpha/2, df=n-1)*se, mean(x)+qt(1-alpha/2, df=n-1)*se)
+        se <- sd(x) / sqrt(n)
+        c(mean(x) - qt(1 - alpha/2, df = n - 1) * se, mean(x) + qt(1 - alpha/2, df = n - 1) * se)
 }
 
 #Plots 95% confidence intervals on linear regression graphs
 plotci <- function(x,y){
-  fit<-lm(y~x)
-  newx<-seq(min(x), max(x), by=0.05)
-  conf_interval <- predict(fit, newdata=data.frame(x=newx), interval="confidence", level=0.95)
-  lines(newx, conf_interval[,2], col="red", lty=2)
-  lines(newx, conf_interval[,3], col="red", lty=2)
+  fit <- lm(y~x)
+  newx <- seq(min(x), max(x), by = 0.05)
+  conf_interval <- predict(fit, newdata = data.frame(x = newx), interval = "confidence", level = 0.95)
+  lines(newx, conf_interval[,2], col = "red", lty = 2)
+  lines(newx, conf_interval[,3], col = "red", lty = 2)
 }
